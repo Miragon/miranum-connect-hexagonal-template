0389b7861a0672485fd5c31af5546788ed75cc00
Rami Nakkar <rami.nakkar@miragon.io>
2023-07-14 08:44:27 +0200

    fix: incorrect arguements

--- example/example-core/src/main/java/io/miragon/example/ExampleAutoConfiguration.java
+++ example/example-core/src/main/java/io/miragon/example/ExampleAutoConfiguration.java
@@ -5,11 +5,11 @@ import io.miragon.example.adapter.out.miranum.MiranumOutAutoConfiguration;
 import io.miragon.example.application.port.in.dosomething.DoSomethingUseCase;
 import io.miragon.example.application.port.in.sendmessage.SendMessageUseCase;
 import io.miragon.example.application.port.in.startprocess.InitiateProcessStartUseCase;
+import io.miragon.example.application.port.out.sendmessage.SendMessagePort;
+import io.miragon.example.application.port.out.startprocess.InitiateProcessStartPort;
 import io.miragon.example.application.service.DoSomethingService;
 import io.miragon.example.application.service.InitiateProcessStartService;
 import io.miragon.example.application.service.SendMessageService;
-import io.miragon.miranum.connect.message.api.MessageApi;
-import io.miragon.miranum.connect.process.impl.StartProcessPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -27,12 +27,12 @@ public class ExampleAutoConfiguration {
     }
 
     @Bean
-    public SendMessageUseCase sendMessageUseCase(final MessageApi messageApi) {
-        return new SendMessageService(messageApi);
+    public SendMessageUseCase sendMessageUseCase(final SendMessagePort sendMessagePort) {
+        return new SendMessageService(sendMessagePort);
     }
 
     @Bean
-    public InitiateProcessStartUseCase initiateProcessStartUseCase(final StartProcessPort startProcessPort) {
-        return new InitiateProcessStartService(startProcessPort);
+    public InitiateProcessStartUseCase initiateProcessStartUseCase(final InitiateProcessStartPort initiateProcessStartPort) {
+        return new InitiateProcessStartService(initiateProcessStartPort);
     }
 }
